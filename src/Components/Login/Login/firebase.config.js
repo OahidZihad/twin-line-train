b5b1823bd73a5b46aae10fd77bf4cad9b9f1bffe
OahidZihad <oahidzihad1@gmail.com>
2021-09-22 17:41:42 +0600

    change firebaseConfig for another email

--- src/Components/Login/Login/firebase.config.js
+++ src/Components/Login/Login/firebase.config.js
@@ -1,10 +1,10 @@
 const firebaseConfig = {
-  apiKey: "AIzaSyApmTqZ1Wq7muiaIlS01ouOCYny-V4za2I",
-  authDomain: "twin-line.firebaseapp.com",
-  projectId: "twin-line",
-  storageBucket: "twin-line.appspot.com",
-  messagingSenderId: "959874969921",
-  appId: "1:959874969921:web:7a9f183f78618d3ace71c0",
+  apiKey: "AIzaSyCkyYimEx8LBVo1NKFFo0tqyFFqQXgaeho",
+  authDomain: "twin-line-5b14b.firebaseapp.com",
+  projectId: "twin-line-5b14b",
+  storageBucket: "twin-line-5b14b.appspot.com",
+  messagingSenderId: "641293930700",
+  appId: "1:641293930700:web:2b253471f55eb9c9c67269",
 };
 
 export default firebaseConfig;
